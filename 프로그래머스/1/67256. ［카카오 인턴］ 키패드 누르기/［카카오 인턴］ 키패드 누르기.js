af1d93e2e991e52a191227780e97e8d22d76827a
Soyeong Park <108514467+soyeong0115@users.noreply.github.com>
2026-04-02 00:16:26 +0900

    [level 1] Title: [카카오 인턴] 키패드 누르기, Time: 0.84 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.js
+++ 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.js
@@ -1,30 +1,37 @@
 function solution(numbers, hand) {
-  const pos = {
-    1:[0,0],2:[0,1],3:[0,2],
-    4:[1,0],5:[1,1],6:[1,2],
-    7:[2,0],8:[2,1],9:[2,2],
-    '*':[3,0],0:[3,1],'#':[3,2]
-  };
-
-  let L = pos['*'], R = pos['#'], ans = '';
-
-  for (let n of numbers) {
-    if ([1,4,7].includes(n)) {
-      ans += 'L'; L = pos[n];
-    } else if ([3,6,9].includes(n)) {
-      ans += 'R'; R = pos[n];
-    } else {
-      const [x,y] = pos[n];
-      const l = Math.abs(L[0]-x) + Math.abs(L[1]-y);
-      const r = Math.abs(R[0]-x) + Math.abs(R[1]-y);
-
-      if (l < r || (l === r && hand === 'left')) {
-        ans += 'L'; L = pos[n];
-      } else {
-        ans += 'R'; R = pos[n];
-      }
+    const keypad = {
+        1:[0,0], 2:[0,1], 3:[0,2],
+        4:[1,0], 5:[1,1], 6:[1,2],
+        7:[2,0], 8:[2,1], 9:[2,2],
+        '*':[3,0], 0:[3,1], '#':[3,2]
+    };
+    
+    let L = keypad['*'], R = keypad['#'], result = '';
+    
+    for (let n of numbers) {
+        if ([1,4,7].includes(n)) {
+            result += 'L';
+            L = keypad[n];
+        } else if ([3,6,9].includes(n)) {
+            result += 'R';
+            R = keypad[n];
+        } else {
+            // 누를 숫자의 위치 꺼내기
+            // [x, y] = 배열 쪼개 받기 (구조분해할당)
+            // x = ~ , y = ~
+            const [x,y] = keypad[n];
+            
+            const l = Math.abs(L[0] - x) + Math.abs(L[1] - y);
+            const r = Math.abs(R[0] - x) + Math.abs(R[1] - y);
+            
+            if (l < r || (l === r && hand === 'left')) {
+                result += 'L';
+                L = keypad[n];
+            } else {
+                result += 'R';
+                R = keypad[n];
+            }
+        }
     }
-  }
-
-  return ans;
+    return result;
 }
