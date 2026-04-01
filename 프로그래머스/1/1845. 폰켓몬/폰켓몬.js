996348d02555e3a568ab5de17668f5e51591786b
Soyeong Park <108514467+soyeong0115@users.noreply.github.com>
2026-04-01 20:22:17 +0900

    [level 1] Title: 폰켓몬, Time: 0.57 ms, Memory: 34.2 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
@@ -1,5 +1,5 @@
 function solution(nums) {
-    const unique = new Set(nums);
+    unique = new Set(nums);
     
-    return Math.min(unique.size, nums.length / 2);
+    return Math.min(unique.size, nums.length / 2)
 }
