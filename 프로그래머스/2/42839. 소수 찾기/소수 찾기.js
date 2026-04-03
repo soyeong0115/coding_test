05268fdcd228ce963c857fffdb94dba356a47099
Soyeong Park <mb535622@gmail.com>
2026-04-03 13:31:16 +0900

    [level 2] Title: 소수 찾기, Time: 37.78 ms, Memory: 37.5 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.js
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.js
@@ -2,22 +2,18 @@ function solution(numbers) {
     const nums = numbers.split("");
     const set = new Set();
     
-    // 순열 만들기
-    function permute(arr, current) {
-        // 지금까지 뽑은 것 먼저 저장
+    function permute (arr, current) {
         if (current.length > 0) {
             set.add(Number(current.join("")));
-        }
+        }        
         for (let i = 0; i < arr.length; i++) {
             const rest = [...arr.slice(0, i), ...arr.slice(i + 1)];
-            // 스프레드 연산자 사용 -> current 배열의 내용물만 꺼내서 넣기
             permute(rest, [...current, arr[i]]);
         }
     }
     
     permute(nums, []);
     
-    // 소수 판별
     function isPrime(n) {
         if (n < 2) return false;
         for (let i = 2; i <= Math.sqrt(n); i++) {
