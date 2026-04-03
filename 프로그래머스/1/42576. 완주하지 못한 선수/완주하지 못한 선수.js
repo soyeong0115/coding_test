e7639aaa0cb4eb03fdd9f4565c871c68ef9e69e1
Soyeong Park <mb535622@gmail.com>
2026-04-03 14:02:27 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 138.99 ms, Memory: 81.7 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,10 +1,13 @@
 function solution(participant, completion) {
-    participant.sort();
-    completion.sort();
+    const map = {};
     
-    for (let i = 0; i < participant.length; i++) {
-        if (participant[i] !== completion[i]) {
-            return participant[i];
-        }
+    for (let name of participant) {
+        map[name] = (map[name] || 0) + 1;
     }
+    
+    for (let name of completion) {
+        map[name]--;
+    }
+    
+    return Object.entries(map).find(([k, v]) => v > 0)[0];
 }
